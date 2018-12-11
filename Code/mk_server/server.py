08015547c5c03b5243a46dd647da0d318f9e910a
Chris Watson <2190594w@student.gla.ac.uk>
2018-12-11 23:15:56 +0000

    Updated master key server to record global attributes in use and added route for retrieving them. Updated index to link to said GET route.

--- Code/mk_server/server.py
+++ Code/mk_server/server.py
@@ -1,4 +1,4 @@
-import pyopenabe
+import json, jsonpickle, pyopenabe
 # TODO: Consider dropping templates, for heightened security (sessions too)
 from io import BytesIO
 from sys import exc_info
@@ -6,13 +6,14 @@ from os import urandom, path
 from datetime import datetime
 from functools import partial
 from flask import Flask, flash, session, request, redirect,\
-send_file, render_template, jsonify
-
+send_file, render_template, jsonify, abort
 
 VERSION = 'v0.0.1'
 
 MASTER_SECRET_KEY_FILE = 'master_secret_key.key'
 MASTER_PUBLIC_KEY_FILE = 'master_public_key.key'
+
+GLOBAL_ABE_ATTRS_FILE = 'global_attrs.config'
 
 app = Flask(__name__)
 
@@ -53,6 +54,17 @@ except:
         app.logger.error("Unexpected error:", exc_info()[0])
         exit()
 
+try:
+    with open(GLOBAL_ABE_ATTRS_FILE, 'r') as gaaf:
+        GLOBAL_ABE_ATTRS_JSON_PICKLED = gaaf.read()
+    GLOBAL_ABE_ATTRS = jsonpickle.decode(GLOBAL_ABE_ATTRS_JSON_PICKLED)
+    GLOBAL_ABE_ATTRS_GENERATED = datetime.fromtimestamp(path.getmtime(GLOBAL_ABE_ATTRS_FILE))
+except:
+    app.logger.error("Unable to recover global attributes file. Generating blank template...")
+    GLOBAL_ABE_ATTRS = {'strings': set(), 'integers': set(), 'dates': set(), 'arrays': set(), 'flags': set()}
+    GLOBAL_ABE_ATTRS_JSON_PICKLED = jsonpickle.encode(GLOBAL_ABE_ATTRS)
+    GLOBAL_ABE_ATTRS_GENERATED = datetime.now()
+
 del cpabe_instance, openabe
 
 @app.route('/')
@@ -69,26 +81,53 @@ def get_public_key():
     }
     return jsonify(mpk_payload)
 
+@app.route('/abe/get_attributes')
+def get_attributes():
+    new_attrs = GLOBAL_ABE_ATTRS
+    for attr in new_attrs:
+        new_attrs[attr] = list(new_attrs[attr])
+    attributes_payload = {
+        'generated_at': GLOBAL_ABE_ATTRS_GENERATED,
+        'updated_at': datetime.now(),
+        'attributes': new_attrs,
+        'abe_version': VERSION
+    }
+    return jsonify(attributes_payload)
+
 def assign_value_key(type, key, value):
-    policy_separators = {"strings": ":", "integers": "=", "dates": " = ", "arrays": ":"}
-    separator = policy_separators[type]
+    attr_separators = {"strings": ":", "integers": "=", "dates": " = ", "arrays": ":"}
+    separator = attr_separators[type]
     value = ",".join(map(str, value)) if type == "arrays" else value
     return f"{key}{separator}{value}"
 
-def create_policy(attrs):
-    user_policy = "|"
+def create_attr_list(attrs):
+    user_attr_list = "|"
     for attr_type, attrs in attrs.items():
         if attr_type in ('strings','integers','dates','arrays'):
             formatted_attrs = list(map(partial(assign_value_key, attr_type), attrs.keys(), attrs.values()))
-            user_policy += "|".join(formatted_attrs) + "|"
-        elif attr_type == "flags":
-            user_policy += "|".join(attrs) + "|"
+            user_attr_list += "|".join(formatted_attrs) + "|"
+        elif attr_type == 'flags':
+            user_attr_list += "|".join(attrs) + "|"
         else:
             pass
-    return user_policy
+    return user_attr_list
 
-@app.route('/abe/generate_userkey/json', methods=['POST'])
-def generate_userkey_json():
+def update_global_attrs(attrs):
+    for attr_type, attrs in attrs.items():
+        if attr_type in ('strings','integers','dates','arrays','flags'):
+            for key in attrs:
+                GLOBAL_ABE_ATTRS[attr_type].add(key)
+    if GLOBAL_ABE_ATTRS_JSON_PICKLED != jsonpickle.encode(GLOBAL_ABE_ATTRS):
+        GLOBAL_ABE_ATTRS_JSON_PICKLED = jsonpickle.encode(GLOBAL_ABE_ATTRS)
+        with open(GLOBAL_ABE_ATTRS_FILE, 'w') as gaaf:
+            gaaf.write(GLOBAL_ABE_ATTRS_JSON_PICKLED)
+        GLOBAL_ABE_ATTRS_GENERATED = datetime.now()
+
+
+@app.route('/abe/generate_userkey/<string:file_or_json>', methods=['POST'])
+def generate_userkey(file_or_json):
+    if file_or_json not in ("file", "json"):
+        abort(404)
     openabe = pyopenabe.PyOpenABE()
     cpabe_instance = openabe.CreateABEContext("CP-ABE")
     cpabe_instance.importSecretParams(MASTER_SECRET_KEY)
@@ -98,11 +137,13 @@ def generate_userkey_json():
     if user_attrs is not None:
         if all(key in user_attrs for key in ('attributes', 'username')):
             str_username = str(user_attrs['username'])
-            user_policy = create_policy(user_attrs['attributes'])
-            print(user_policy)
+            attrs = user_attrs['attributes']
+            update_global_attrs(attrs)
+            user_attr_list = create_attr_list(attrs)
+            print(user_attr_list)
             del user_attrs
             try:
-                cpabe_instance.keygen(user_policy, str_username)
+                cpabe_instance.keygen(user_attr_list, str_username)
                 print("successfully generated key")
                 userkey_generated_at = datetime.now()
                 userkey = cpabe_instance.exportUserKey(str_username)
@@ -110,51 +151,22 @@ def generate_userkey_json():
                 app.logger.error("Fatal error during key generation")
                 return jsonify({'msg': 'FATAL ERROR'}), 500
 
-            userkey_payload = {
-                'filename': f"{str_username}.key",
-                'generated_at': userkey_generated_at,
-                'updated_at': datetime.now(),
-                'userkey': userkey.decode('UTF-8'),
-                'abe_version': VERSION
-            }
-            return jsonify(userkey_payload)
-    return jsonify({'msg': 'Improper JSON values. Ensure both "attributes" and "username" keys are present!'}), 422
-
-@app.route('/abe/generate_userkey/file', methods=['POST'])
-def generate_userkey_file():
-    openabe = pyopenabe.PyOpenABE()
-    cpabe_instance = openabe.CreateABEContext("CP-ABE")
-    cpabe_instance.importSecretParams(MASTER_SECRET_KEY)
-    cpabe_instance.importPublicParams(MASTER_PUBLIC_KEY)
-    print("imported")
-    user_attrs = request.get_json()
-    if user_attrs is not None:
-        if all(key in user_attrs for key in ('attributes', 'username')):
-            str_username = str(user_attrs['username'])
-            user_policy = create_policy(user_attrs['attributes'])
-            print(user_policy)
-            del user_attrs
-            try:
-                cpabe_instance.keygen(user_policy, str_username)
-                print("successfully generated key")
-                userkey_generated_at = datetime.now()
-                userkey = cpabe_instance.exportUserKey(str_username)
-            except:
-                app.logger.error("Fatal error during key generation")
-                return jsonify({'msg': 'FATAL ERROR'}), 500
-
-            userkey_payload = {
-                'filename': f"{str_username}.key",
-                'generated_at': userkey_generated_at,
-                'updated_at': datetime.now(),
-                'userkey': userkey.decode('UTF-8'),
-                'abe_version': VERSION
-            }
-            return send_file(
-                BytesIO(userkey),
-                mimetype='text/plain',
-                as_attachment=True,
-                attachment_filename=f"{str_username}.key")
+            if file_or_json == "json":
+                userkey_payload = {
+                    'filename': f"{str_username}.key",
+                    'generated_at': userkey_generated_at,
+                    'updated_at': datetime.now(),
+                    'userkey': userkey.decode('UTF-8'),
+                    'user_attrs': user_attr_list,
+                    'abe_version': VERSION
+                }
+                return jsonify(userkey_payload)
+            else:
+                return send_file(
+                    BytesIO(userkey),
+                    mimetype='text/plain',
+                    as_attachment=True,
+                    attachment_filename=f"{str_username}.key")
     return jsonify({'msg': 'Improper JSON values. Ensure both "attributes" and "username" keys are present!'}), 422
 
 @app.errorhandler(404)
