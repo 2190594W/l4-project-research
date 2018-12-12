61d819e1a72b482ea35581196174c5a0f17166b8
Chris Watson <2190594w@student.gla.ac.uk>
2018-12-12 00:51:54 +0000

    Added routes for serving latest attributes universe and mpk. Updated index for new routes. Imports tidy up. Renamed routes to better fit design. Added config for mpk and attributes. Added dev env retrieval of mpk & attrs from localhost msk server.

--- Code/res_server/server.py
+++ Code/res_server/server.py
@@ -1,4 +1,6 @@
-import os, sys, pyopenabe
+import json, requests, pyopenabe
+from sys import exc_info
+from os import urandom, path, walk
 from datetime import datetime
 from flask import Flask, flash, session, request, redirect, render_template,\
 url_for, send_from_directory, jsonify
@@ -9,21 +11,73 @@ ALLOWED_EXTENSIONS = set(['cpabe', 'jp2'])
 
 VERSION = 'v0.0.1'
 
+MASTER_PUBLIC_KEY_FILE = 'master_public_key.key'
+
+GLOBAL_ABE_ATTRS_FILE = 'global_attrs.config'
+
 app = Flask(__name__)
 
 try:
     with open('session_secret.config', 'rb') as session_secret:
         app.secret_key = session_secret.read().strip()
         if len(app.secret_key) > 32:
-            app.logger.info("Configured session secret")
+            app.logger.info("Collected session secret from config file.")
         else:
             raise ValueError("Insufficient session secret provided")
 except:
     app.logger.error("No sufficient session secret found, auto-generated random 64-bytes.")
-    app.secret_key = os.urandom(64)
+    app.secret_key = urandom(64)
 
 app.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+try:
+    with open(MASTER_PUBLIC_KEY_FILE, 'rb') as mpkf:
+        MASTER_PUBLIC_KEY = mpkf.read()
+    MASTER_KEY_UPDATED = datetime.fromtimestamp(path.getmtime(MASTER_PUBLIC_KEY_FILE))
+    app.logger.info("Collected master public key from config file.")
+except:
+    try:
+        response = requests.get('http://localhost:5000/abe/get_public_key')
+        if response.status_code == 200:
+            res_dict = json.loads(response.content)
+            MASTER_PUBLIC_KEY = res_dict['mpk'].encode('UTF-8')
+            with open(MASTER_PUBLIC_KEY_FILE, 'wb') as mpkf:
+                mpkf.write(MASTER_PUBLIC_KEY)
+            MASTER_KEY_UPDATED = datetime.now()
+            app.logger.info("DEV: Collected master public key from MSK server.")
+        else:
+            raise Exception('Unable to reach dev msk server.')
+    except:
+        app.logger.error("FATAL ERROR: ABORTING SERVER")
+        app.logger.error(f"Cannot run server without global attributes file: {MASTER_PUBLIC_KEY_FILE}")
+        app.logger.error("Unexpected error:", exc_info()[0])
+        exit()
+
+try:
+    with open(GLOBAL_ABE_ATTRS_FILE, 'r') as gaaf:
+        GLOBAL_ABE_ATTRS_JSON = gaaf.read()
+    GLOBAL_ABE_ATTRS = json.loads(GLOBAL_ABE_ATTRS_JSON)
+    GLOBAL_ABE_ATTRS_UPDATED = datetime.fromtimestamp(path.getmtime(GLOBAL_ABE_ATTRS_FILE))
+    app.logger.info("Collected attributes from config file.")
+except:
+    try:
+        response = requests.get('http://localhost:5000/abe/get_attributes')
+        if response.status_code == 200:
+            res_dict = json.loads(response.content)
+            GLOBAL_ABE_ATTRS = res_dict['attributes']
+            GLOBAL_ABE_ATTRS_JSON = json.dumps(GLOBAL_ABE_ATTRS)
+            with open(GLOBAL_ABE_ATTRS_FILE, 'w') as gaaf:
+                gaaf.write(GLOBAL_ABE_ATTRS_JSON)
+            GLOBAL_ABE_ATTRS_UPDATED = datetime.now()
+            app.logger.info("DEV: Collected attributes from MSK server.")
+        else:
+            raise Exception('Unable to reach dev msk server.')
+    except:
+        app.logger.error("FATAL ERROR: ABORTING SERVER")
+        app.logger.error(f"Cannot run server without global attributes file: {GLOBAL_ABE_ATTRS_FILE}")
+        app.logger.error("Unexpected error:", exc_info()[0])
+        exit()
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -51,7 +105,7 @@ def upload_file():
         if file and allowed_file(file.filename):
             flash('successfully uploaded!')
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            file.save(path.join(app.config['UPLOAD_FOLDER'], filename))
             return redirect(url_for('uploaded_file',
                                     filename=filename))
         flash('Error with upload!', 'error')
@@ -65,10 +119,10 @@ def uploaded_file(filename):
 
 # TODO: This is a näive implementation. If there are too many files,
 # the response would be massive, so paging would be needed here
-@app.route('/get_filenames')
-def get_filenames():
+@app.route('/all_filenames')
+def get_all_filenames():
     all_files = []
-    for root, dirs, files in os.walk(app.config['UPLOAD_FOLDER']):
+    for root, dirs, files in walk(app.config['UPLOAD_FOLDER']):
         all_files.extend(files)
     filenames_payload = {
         'files': all_files,
@@ -76,6 +130,26 @@ def get_filenames():
         'abe_version': VERSION
     }
     return jsonify(filenames_payload)
+
+@app.route('/abe/latest_mpk')
+def get_latest_mpk():
+    mpk_payload = {
+        'generated_at': MASTER_KEY_UPDATED,
+        'updated_at': datetime.now(),
+        'mpk': MASTER_PUBLIC_KEY.decode('UTF-8'),
+        'abe_version': VERSION
+    }
+    return jsonify(mpk_payload)
+
+@app.route('/abe/latest_attributes')
+def get_latest_attributes():
+    attributes_payload = {
+        'generated_at': GLOBAL_ABE_ATTRS_UPDATED,
+        'updated_at': datetime.now(),
+        'attributes': GLOBAL_ABE_ATTRS,
+        'abe_version': VERSION
+    }
+    return jsonify(attributes_payload)
 
 @app.errorhandler(404)
 def page_not_found(e):
