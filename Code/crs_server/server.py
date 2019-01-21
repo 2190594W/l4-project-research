9ae807268da0197e24325c72670917c1bdd6fd0e
Chris Watson <2190594w@student.gla.ac.uk>
2019-01-21 22:47:52 +0000

    Fixed false-positive pylint error with logging tool. Implemented decrypt and decrypt&download views. Fixed encrypt and encrypt&upload views. Improved file extension checking system. Re-ordered imports for pylint. Improved try...excepts for IO tasks. Fixed lazy overwriting of var names. Where prompted split lines over 100 chars to multiline. Improved flash messages across the server. Fixed issue with openabe and cpabe instances quietly crashing the server. Added a download view. Fixed 404 page warning. Ensured the new decrypt view also supports uploading of a user key, instead of relying on the hardcoded key.

--- Code/crs_server/server.py
+++ Code/crs_server/server.py
@@ -8,12 +8,15 @@ from io import BytesIO
 from datetime import datetime
 import json
 import requests
-import pyopenabe
 from werkzeug.utils import secure_filename
 from flask import Flask, flash, request, redirect, render_template,\
-url_for, send_from_directory, send_file, jsonify
+url_for, send_file, jsonify
+from flask.logging import create_logger
+import pyopenabe
 
-ALLOWED_EXTENSIONS = set(['jp2', 'docx', 'txt', 'pdf'])
+ENC_ALLOWED_EXTENSIONS = set(['jp2', 'docx', 'txt', 'pdf', 'zip'])
+DEC_ALLOWED_EXTENSIONS = set(['cpabe'])
+KEY_ALLOWED_EXTENSIONS = set(['key'])
 
 VERSION = 'v0.0.1'
 RES_SERVER = "localhost:5001"
@@ -21,105 +24,108 @@ RES_SERVER = "localhost:5001"
 MASTER_PUBLIC_KEY_FILE = 'master_public_key.key'
 GLOBAL_ABE_ATTRS_FILE = 'global_attrs.config'
 
-USERNAME = "testuser"
+USERNAME = "2637216p"
+
+APP = Flask(__name__)
+LOG = create_logger(APP)
 
 try:
     with open('userkey.key', 'rb') as userkey:
         USERKEY = userkey.read().strip()
-except:
-    app.logger.error("No user key found!")
+except EnvironmentError:
+    LOG.error("No user key found!")
     exit()
-
-app = Flask(__name__)
 
 try:
     with open('session_secret.config', 'rb') as session_secret:
-        app.secret_key = session_secret.read().strip()
-        if len(app.secret_key) > 32:
-            app.logger.info("Collected session secret from config file.")
+        APP.secret_key = session_secret.read().strip()
+        if len(APP.secret_key) > 32:
+            LOG.info("Collected session secret from config file.")
         else:
             raise ValueError("Insufficient session secret provided")
-except:
-    app.logger.error("No sufficient session secret found, auto-generated random 64-bytes.")
-    app.secret_key = urandom(64)
+except EnvironmentError:
+    LOG.error("No sufficient session secret found, auto-generated random 64-bytes.")
+    APP.secret_key = urandom(64)
 
-app.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024
+APP.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024
 
-def get_latest_mpk(mpkf):
+def get_latest_mpk(mpk_f):
     response = requests.get(f'http://{RES_SERVER}/abe/latest_mpk')
     if response.status_code == 200:
         res_dict = json.loads(response.content)
         mpk = res_dict['mpk'].encode('UTF-8')
-        with open(mpkf, 'wb') as mpk_f:
-            mpk_f.write(mpk)
+        with open(mpk_f, 'wb') as mpk_b:
+            mpk_b.write(mpk)
         mku = datetime.now()
-        app.logger.info("DEV: Collected master public key from RES server.")
+        LOG.info("DEV: Collected master public key from RES server.")
         return mpk, mku
-    else:
-        raise Exception('Unable to reach dev res server.')
+    raise EnvironmentError('Unable to reach dev res server.')
 
 try:
     with open(MASTER_PUBLIC_KEY_FILE, 'rb') as mpkf:
         MASTER_PUBLIC_KEY = mpkf.read()
     MASTER_KEY_UPDATED = datetime.fromtimestamp(path.getmtime(MASTER_PUBLIC_KEY_FILE))
-    app.logger.info("Collected master public key from config file.")
-except:
+    LOG.info("Collected master public key from config file.")
+except EnvironmentError:
     try:
         MASTER_PUBLIC_KEY, MASTER_KEY_UPDATED = get_latest_mpk(MASTER_PUBLIC_KEY_FILE)
-    except:
-        app.logger.error("FATAL ERROR: ABORTING SERVER")
-        app.logger.error(f"Cannot run server without global attributes file: {MASTER_PUBLIC_KEY_FILE}")
-        app.logger.error("Unexpected error:", exc_info()[0])
+    except EnvironmentError:
+        LOG.error("FATAL ERROR: ABORTING SERVER")
+        LOG.error(f"Cannot run server without global attributes file: {MASTER_PUBLIC_KEY_FILE}")
+        LOG.error("Unexpected error:", exc_info()[0])
         exit()
 
-def get_latest_attributes(gaaf):
+def get_latest_attributes(gaa_f):
     response = requests.get(f'http://{RES_SERVER}/abe/latest_attributes')
     if response.status_code == 200:
         res_dict = json.loads(response.content)
         gaa = res_dict['attributes']
         gaaj = json.dumps(gaa)
-        with open(gaaf, 'w') as gaa_f:
-            gaa_f.write(gaaj)
+        with open(gaa_f, 'w') as gaa_b:
+            gaa_b.write(gaaj)
         gaau = datetime.now()
-        app.logger.info("DEV: Collected attributes from RES server.")
+        LOG.info("DEV: Collected attributes from RES server.")
         return gaa, gaaj, gaau
-    else:
-        raise Exception('Unable to reach dev res server.')
+    raise EnvironmentError('Unable to reach dev res server.')
 
 try:
     with open(GLOBAL_ABE_ATTRS_FILE, 'r') as gaaf:
         GLOBAL_ABE_ATTRS_JSON = gaaf.read()
     GLOBAL_ABE_ATTRS = json.loads(GLOBAL_ABE_ATTRS_JSON)
     GLOBAL_ABE_ATTRS_UPDATED = datetime.fromtimestamp(path.getmtime(GLOBAL_ABE_ATTRS_FILE))
-    app.logger.info("Collected attributes from config file.")
-except:
+    LOG.info("Collected attributes from config file.")
+except EnvironmentError:
     try:
-        GLOBAL_ABE_ATTRS, GLOBAL_ABE_ATTRS_JSON, GLOBAL_ABE_ATTRS_UPDATED = get_latest_attributes(GLOBAL_ABE_ATTRS_FILE)
-    except:
-        app.logger.error("FATAL ERROR: ABORTING SERVER")
-        app.logger.error(f"Cannot run server without global attributes file: {GLOBAL_ABE_ATTRS_FILE}")
-        app.logger.error("Unexpected error:", exc_info()[0])
+        GLOBAL_ABE_ATTRS, GLOBAL_ABE_ATTRS_JSON, GLOBAL_ABE_ATTRS_UPDATED \
+            = get_latest_attributes(GLOBAL_ABE_ATTRS_FILE)
+    except EnvironmentError:
+        LOG.error("FATAL ERROR: ABORTING SERVER")
+        LOG.error(f"Cannot run server without global attributes file: \
+            {GLOBAL_ABE_ATTRS_FILE}")
+        LOG.error("Unexpected error:", exc_info()[0])
         exit()
 
-def allowed_file(filename):
+def allowed_file(filename, ALLOWED_EXTENSIONS):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/')
+@APP.route('/')
 def hello_world():
     return render_template('index.html')
 
-@app.route('/abe/check_mpk_updated')
+@APP.route('/abe/check_mpk_updated')
 def check_mpk_updated():
     global MASTER_PUBLIC_KEY, MASTER_KEY_UPDATED
     mpk2, mku2 = get_latest_mpk(MASTER_PUBLIC_KEY_FILE)
     if mpk2 != MASTER_PUBLIC_KEY:
         MASTER_PUBLIC_KEY = mpk2
         MASTER_KEY_UPDATED = mku2
-        return render_template('mpk_updated.html', updated=True, mpk=MASTER_PUBLIC_KEY.decode('UTF-8'), mku=MASTER_KEY_UPDATED)
-    return render_template('mpk_updated.html', updated=False, mpk=MASTER_PUBLIC_KEY, mku=MASTER_KEY_UPDATED)
+        return render_template('mpk_updated.html', updated=True,\
+            mpk=MASTER_PUBLIC_KEY.decode('UTF-8'), mku=MASTER_KEY_UPDATED)
+    return render_template('mpk_updated.html', updated=False,\
+        mpk=MASTER_PUBLIC_KEY, mku=MASTER_KEY_UPDATED)
 
-@app.route('/abe/latest_mpk_file')
+@APP.route('/abe/latest_mpk_file')
 def latest_mpk_file():
     return send_file(
         BytesIO(MASTER_PUBLIC_KEY),
@@ -127,7 +133,7 @@ def latest_mpk_file():
         as_attachment=True,
         attachment_filename="master_public_key.key")
 
-@app.route('/abe/latest_mpk_json')
+@APP.route('/abe/latest_mpk_json')
 def latest_mpk_json():
     mpk_payload = {
         'generated_at': MASTER_KEY_UPDATED,
@@ -137,7 +143,7 @@ def latest_mpk_json():
     }
     return jsonify(mpk_payload)
 
-@app.route('/abe/check_attributes_updated')
+@APP.route('/abe/check_attributes_updated')
 def check_attributes_updated():
     global GLOBAL_ABE_ATTRS, GLOBAL_ABE_ATTRS_JSON, GLOBAL_ABE_ATTRS_UPDATED
     gaa2, gaaj2, gaau2 = get_latest_attributes(GLOBAL_ABE_ATTRS_FILE)
@@ -148,8 +154,7 @@ def check_attributes_updated():
         return render_template('attrs_updated.html', updated=True, gaaj=GLOBAL_ABE_ATTRS_JSON, gaau=GLOBAL_ABE_ATTRS_UPDATED)
     return render_template('attrs_updated.html', updated=False, gaaj=GLOBAL_ABE_ATTRS_JSON, gaau=GLOBAL_ABE_ATTRS_UPDATED)
 
-
-@app.route('/abe/latest_attributes_file')
+@APP.route('/abe/latest_attributes_file')
 def latest_attributes_file():
     return send_file(
         BytesIO(GLOBAL_ABE_ATTRS_JSON.encode('UTF-8')),
@@ -157,7 +162,7 @@ def latest_attributes_file():
         as_attachment=True,
         attachment_filename="global_attrs.config")
 
-@app.route('/abe/latest_attributes_json')
+@APP.route('/abe/latest_attributes_json')
 def latest_attributes_json():
     attributes_payload = {
         'generated_at': GLOBAL_ABE_ATTRS_UPDATED,
@@ -167,7 +172,7 @@ def latest_attributes_json():
     }
     return jsonify(attributes_payload)
 
-@app.route('/upload', methods=['GET', 'POST'])
+@APP.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
@@ -180,7 +185,8 @@ def upload_file():
         if file.filename == '':
             flash('No selected file', 'info')
             return redirect(request.url)
-        if file and allowed_file(file.filename):
+        if file and allowed_file(file.filename, ENC_ALLOWED_EXTENSIONS):
+            file.filename = secure_filename(file.filename)
             files = {'file': file}
             res_server_res = requests.post(f'http://{RES_SERVER}/upload', files=files)
             if res_server_res.status_code == 200:
@@ -189,12 +195,15 @@ def upload_file():
         flash('Error with upload!', 'error')
     return render_template('upload.html')
 
-@app.route('/encrypt_upload', methods=['GET', 'POST'])
-def encrypt_upload_file():
+@APP.route('/encrypt', methods=['GET', 'POST'])
+def encrypt_file():
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
             flash('No file part', 'info')
+            return redirect(request.url)
+        if 'policy' not in request.form:
+            flash('No policy provided!', 'info')
             return redirect(request.url)
         file = request.files['file']
         policy = request.form['policy']
@@ -203,30 +212,150 @@ def encrypt_upload_file():
         if file.filename == '':
             flash('No selected file', 'info')
             return redirect(request.url)
-        if file and policy and allowed_file(file.filename):
+        if file and policy and allowed_file(file.filename, ENC_ALLOWED_EXTENSIONS):
+            file_filename = secure_filename(file.filename)
             openabe = pyopenabe.PyOpenABE()
             cpabe = openabe.CreateABEContext("CP-ABE")
             cpabe.importPublicParams(MASTER_PUBLIC_KEY)
-            cpabe.importUserKey(USERNAME, USERKEY)
-            ct_file = cpabe.encrypt(policy, file.read())
+            try:
+                ct_file = cpabe.encrypt(policy, file.read())
+            except pyopenabe.PyOpenABEError as err:
+                del openabe, cpabe
+                flash(f"PyOpenABEError: {err}", 'error')
+                return render_template('encrypt.html')
+            del openabe, cpabe
+            file_filename += '.cpabe'
+            return send_file(
+                BytesIO(ct_file),
+                mimetype='text/plain',
+                as_attachment=True,
+                attachment_filename=file_filename)
+        flash('Error with encryption!', 'error')
+    return render_template('encrypt.html')
+
+@APP.route('/encrypt_upload', methods=['GET', 'POST'])
+def encrypt_upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file uploaded!', 'info')
+            return redirect(request.url)
+        if 'policy' not in request.form:
+            flash('No policy provided!', 'info')
+            return redirect(request.url)
+        file = request.files['file']
+        policy = request.form['policy']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file', 'info')
+            return redirect(request.url)
+        if file and policy and allowed_file(file.filename, ENC_ALLOWED_EXTENSIONS):
+            file.filename = secure_filename(file.filename)
+            openabe = pyopenabe.PyOpenABE()
+            cpabe = openabe.CreateABEContext("CP-ABE")
+            cpabe.importPublicParams(MASTER_PUBLIC_KEY)
+            try:
+                ct_file = cpabe.encrypt(policy, file.read())
+            except pyopenabe.PyOpenABEError as err:
+                del openabe, cpabe
+                flash(f"PyOpenABEError: {err}", 'error')
+                return render_template('encrypt_upload.html')
+            del openabe, cpabe
             file.filename += '.cpabe'
             files = {'file': (file.filename, ct_file, file.content_type)}
             res_server_res = requests.post(f'http://{RES_SERVER}/upload', files=files)
             if res_server_res.status_code == 200:
                 flash('successfully uploaded!', 'info')
-                return redirect(url_for('get_all_filenames'))
+                return redirect(url_for('get_all_filenames'), code=303)
         flash('Error with upload!', 'error')
     return render_template('encrypt_upload.html')
 
+@APP.route('/download/<filename>')
+def download_file(filename):
+    res_server_res = requests.get(f'http://{RES_SERVER}/download/{filename}')
+    if res_server_res.status_code == 200:
+        return send_file(
+            BytesIO(res_server_res.content),
+            mimetype='text/plain',
+            as_attachment=True,
+            attachment_filename=filename[:-6])
+    flash('No file matching that name found', 'error')
+    return render_template('download_fail.html')
 
-@app.route('/download/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(
-        app.config['UPLOAD_FOLDER'], filename)
+@APP.route('/decrypt', methods=['GET', 'POST'])
+def decrypt_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'enc_file' not in request.files:
+            flash('No encrypted file provided!', 'info')
+        elif 'user_key' not in request.files:
+            flash('No user key provided!', 'info')
+        else:
+            enc_file = request.files['enc_file']
+            user_key = request.files['user_key']
+            # if user does not select file, browser also
+            # submit an empty part without filename
+            if enc_file.filename == '':
+                flash('No encrypted file provided!', 'info')
+            elif user_key.filename == '':
+                flash('No user key provided!', 'info')
+            elif enc_file and user_key and allowed_file(enc_file.filename, DEC_ALLOWED_EXTENSIONS)\
+                and allowed_file(user_key.filename, KEY_ALLOWED_EXTENSIONS):
+                file_bytes = enc_file.read()
+                key_bytes = user_key.read().strip()
+                username = user_key.filename.split('.')[0]
+                if file_bytes is not None and key_bytes is not None:
+                    openabe = pyopenabe.PyOpenABE()
+                    cpabe = openabe.CreateABEContext("CP-ABE")
+                    cpabe.importPublicParams(MASTER_PUBLIC_KEY)
+                    cpabe.importUserKey(username, key_bytes)
+                    try:
+                        dec_file = cpabe.decrypt(username, file_bytes)
+                    except pyopenabe.PyOpenABEError as err:
+                        flash(f"Decryption of file failed: {err}", 'error')
+                        dec_file = None
+                    del openabe, cpabe
+                    if dec_file is not None:
+                        return send_file(
+                            BytesIO(dec_file),
+                            mimetype='text/plain',
+                            as_attachment=True,
+                            attachment_filename=enc_file.filename[:-6])
+                flash('Decryption of file failed', 'error')
+            flash('Issue with files! Make sure user key is a .key file and that the\
+                encrypted file is a .cpabe file!', 'error')
+    return render_template('decrypt.html')
+
+@APP.route('/decrypt_download/<filename>')
+def decrypt_download_file(filename):
+    res_server_res = requests.get(f'http://{RES_SERVER}/download/{filename}')
+    if res_server_res.status_code == 200:
+        file_bytes = res_server_res.content
+        if file_bytes is not None:
+            openabe = pyopenabe.PyOpenABE()
+            cpabe = openabe.CreateABEContext("CP-ABE")
+            cpabe.importPublicParams(MASTER_PUBLIC_KEY)
+            cpabe.importUserKey(USERNAME, USERKEY)
+            try:
+                dec_file = cpabe.decrypt(USERNAME, file_bytes)
+            except pyopenabe.PyOpenABEError:
+                dec_file = None
+            del openabe, cpabe
+            if dec_file is not None:
+                return send_file(
+                    BytesIO(dec_file),
+                    mimetype='text/plain',
+                    as_attachment=True,
+                    attachment_filename=filename[:-6])
+        flash('Decryption of file failed', 'error')
+        return render_template('decrypt_download.html', file=filename)
+    flash('No file matching that name found', 'error')
+    return render_template('download_fail.html')
 
 # TODO: This is a näive implementation. If there are too many files,
 # the response would be massive, so paging would be needed here
-@app.route('/all_filenames')
+@APP.route('/all_filenames')
 def get_all_filenames():
     response = requests.get(f'http://{RES_SERVER}/all_filenames')
     all_files = None
@@ -235,6 +364,6 @@ def get_all_filenames():
         all_files = res_dict['files']
     return render_template('all_files.html', files=all_files)
 
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
+@APP.errorhandler(404)
+def page_not_found(event):
+    return render_template('404.html', event=event), 404
