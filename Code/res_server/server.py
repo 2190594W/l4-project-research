2c2f9c273ae27f9c35b053bf2a2db958290cee8f
Chris Watson <2190594w@student.gla.ac.uk>
2019-01-27 18:44:27 +0000

    Added docstrings for Res Server. Restructured global constants for PyLint compliance (added modularity). Fixed spellings.

--- Code/res_server/server.py
+++ Code/res_server/server.py
@@ -24,6 +24,7 @@ UPLOAD_FOLDER = '/tmp/flask/file/uploads'
 ALLOWED_EXTENSIONS = set(['cpabe'])
 
 VERSION = 'v0.0.2'
+MK_SERVER = 'localhost:5000'
 
 MASTER_PUBLIC_KEY_FILE = 'master_public_key.key'
 
@@ -46,6 +47,36 @@ except EnvironmentError:
 APP.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024
 APP.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+def update_mpk_file(mk_server, mpk_file_path):
+    """Function to process updating the given MASTER PUBLIC KEY (MPK)
+    file with the up-to-date values retrieved from the given MK SERVER address.
+
+    Parameters
+    ----------
+    mk_server : string
+        String representing the address of the `mk_server`.
+    mpk_file_path : string
+        String representing the path of the MPK file to update `mpk_file_path`.
+
+    Returns
+    -------
+    Bytes, datetime
+        Returns:
+            Bytes object representing the current MPK retrieved.
+            Datetime object representing the time the MPK was retrieved.
+
+    """
+    mpk_res = requests.get(f'http://{mk_server}/abe/get_public_key')
+    mpk_u = datetime.now()
+    if mpk_res.status_code == 200:
+        mpk_res_dict = json.loads(mpk_res.content)
+        mpk = mpk_res_dict['mpk'].encode('UTF-8')
+        with open(mpk_file_path, 'wb') as mpk_f:
+            mpk_f.write(mpk)
+        LOG.info("DEV: Collected master public key from MSK server.")
+        return mpk, mpk_u
+    raise EnvironmentError('Unable to reach dev msk server.')
+
 try:
     with open(MASTER_PUBLIC_KEY_FILE, 'rb') as mpkf:
         MASTER_PUBLIC_KEY = mpkf.read()
@@ -53,21 +84,44 @@ try:
     LOG.info("Collected master public key from config file.")
 except EnvironmentError:
     try:
-        response = requests.get('http://localhost:5000/abe/get_public_key')
-        if response.status_code == 200:
-            res_dict = json.loads(response.content)
-            MASTER_PUBLIC_KEY = res_dict['mpk'].encode('UTF-8')
-            with open(MASTER_PUBLIC_KEY_FILE, 'wb') as mpkf:
-                mpkf.write(MASTER_PUBLIC_KEY)
-            MASTER_KEY_UPDATED = datetime.now()
-            LOG.info("DEV: Collected master public key from MSK server.")
-        else:
-            raise Exception('Unable to reach dev msk server.')
+        MASTER_PUBLIC_KEY, MASTER_KEY_UPDATED = update_mpk_file(MK_SERVER, MASTER_PUBLIC_KEY_FILE)
     except EnvironmentError:
         LOG.error("FATAL ERROR: ABORTING SERVER")
         LOG.error("Cannot run server without global attributes file: %s", MASTER_PUBLIC_KEY_FILE)
         LOG.error("Unexpected error: %s", exc_info()[0])
         exit()
+
+def update_gaa_file(mk_server, gaa_file_path):
+    """Function to process updating the given GLOBAL ABE ATTRS (GAA)
+    file with the up-to-date values retrieved from the given MK SERVER address.
+
+    Parameters
+    ----------
+    mk_server : string
+        String representing the address of the `mk_server`.
+    gaa_file_path : string
+        String representing the path of the GAA file to update `gaa_file_path`.
+
+    Returns
+    -------
+    Dict, string, datetime
+        Returns:
+            Dictionary object representing the current GAA retrieved.
+            String representing the JSON encoded GAA.
+            Datetime object representing the time the GAA were retrieved.
+
+    """
+    gaa_res = requests.get(f'http://{mk_server}/abe/get_attributes')
+    gaa_u = datetime.now()
+    if gaa_res.status_code == 200:
+        gaa_res_dict = json.loads(gaa_res.content)
+        gaa = gaa_res_dict['attributes']
+        gaa_j = json.dumps(gaa)
+        with open(gaa_file_path, 'w') as gaa_f:
+            gaa_f.write(gaa_j)
+        LOG.info("DEV: Collected attributes from MSK server.")
+        return gaa, gaa_j, gaa_u
+    raise EnvironmentError('Unable to reach dev msk server.')
 
 try:
     with open(GLOBAL_ABE_ATTRS_FILE, 'r') as gaaf:
@@ -77,17 +131,8 @@ try:
     LOG.info("Collected attributes from config file.")
 except EnvironmentError:
     try:
-        response = requests.get('http://localhost:5000/abe/get_attributes')
-        if response.status_code == 200:
-            res_dict = json.loads(response.content)
-            GLOBAL_ABE_ATTRS = res_dict['attributes']
-            GLOBAL_ABE_ATTRS_JSON = json.dumps(GLOBAL_ABE_ATTRS)
-            with open(GLOBAL_ABE_ATTRS_FILE, 'w') as gaaf:
-                gaaf.write(GLOBAL_ABE_ATTRS_JSON)
-            GLOBAL_ABE_ATTRS_UPDATED = datetime.now()
-            LOG.info("DEV: Collected attributes from MSK server.")
-        else:
-            raise Exception('Unable to reach dev msk server.')
+        GLOBAL_ABE_ATTRS, GLOBAL_ABE_ATTRS_JSON, GLOBAL_ABE_ATTRS_UPDATED = \
+            update_gaa_file(MK_SERVER, GLOBAL_ABE_ATTRS_FILE)
     except EnvironmentError:
         LOG.error("FATAL ERROR: ABORTING SERVER")
         LOG.error("Cannot run server without global attributes file %s", GLOBAL_ABE_ATTRS_FILE)
@@ -95,15 +140,57 @@ except EnvironmentError:
         exit()
 
 def allowed_file(filename):
+    """Small function to validate that a given filename ends with a valid extension.
+
+    Parameters
+    ----------
+    filename : string
+        String representing the `filename` to be checked.
+
+    Returns
+    -------
+    boolean
+        Boolean value representing if file extension is allowed.
+
+    """
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @APP.route('/')
 def hello_world():
+    """Simple template generation for homepage/index of app.
+    Attached to '/' route by flask annotation.
+
+    Parameters
+    ----------
+
+
+    Returns
+    -------
+    Response (flask)
+        Generates flask Response object from Jinja2 template.
+
+    """
     return render_template('index.html')
 
 @APP.route('/upload', methods=['GET', 'POST'])
 def upload_file():
+    """If GET request, simple template generation for upload page of app.
+    If POST request, handle uploading file in request to the Resource
+    server, while validating file and filename.
+    If successful, redirects to list of uploaded files.
+    Attached to '/upload' route by flask annotation.
+
+    Parameters
+    ----------
+
+
+    Returns
+    -------
+    Response (flask)
+        Generates flask Response object from Jinja2 template.
+
+    """
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
@@ -115,13 +202,13 @@ def upload_file():
         else:
             author = request.form["author"]
         if 'policy' not in request.form:
-            flash('No policy provided for metadat! File will be uploaded without.', 'info')
+            flash('No policy provided for metadata! File will be uploaded without.', 'info')
             policy = None
         else:
             policy = request.form["policy"]
         file = request.files['file']
         # if user does not select file, browser also
-        # submit an empty part without filename
+        # may submit an empty part without filename
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
@@ -154,6 +241,23 @@ def upload_file():
 
 @APP.route('/download/<file_id>')
 def download_cpabe_file(file_id):
+    """View to download an encrypted file for the user.
+    Retrieves an encrypted file from the uploads directory,
+    as defined by the provided file_id param.
+    Attached to '/download/<file_id>' route by flask annotation.
+
+    Parameters
+    ----------
+    file_id : string (UUID)
+        String representing the UUID of the uploaded file `file_id`.
+
+
+    Returns
+    -------
+    Response (flask)
+        Generates flask Response object from a file retrieved from directory.
+
+    """
     file_obj = META_DB.find_one({"id": uuid.UUID(file_id)}, {"filename"})
     if file_obj is not None:
         filename = file_obj["filename"]
@@ -168,6 +272,23 @@ def download_cpabe_file(file_id):
 
 @APP.route('/download_meta/<file_id>')
 def download_meta_file(file_id):
+    """View to download a metadata file for the user.
+    Retrieves a metadata file from the uploads directory,
+    as defined by the provided file_id param.
+    Attached to '/download/<file_id>' route by flask annotation.
+
+    Parameters
+    ----------
+    file_id : string (UUID)
+        String representing the UUID of the metadata file `file_id`.
+
+
+    Returns
+    -------
+    Response (flask)
+        Generates flask Response object from a file retrieved from directory.
+
+    """
     file_obj = META_DB.find_one({"id": uuid.UUID(file_id)}, {"filename"})
     if file_obj is not None:
         filename = file_obj["filename"]
@@ -184,6 +305,22 @@ def download_meta_file(file_id):
 
 @APP.route('/file_meta/<file_id>')
 def get_file_meta(file_id):
+    """View to return the metadata for a file.
+    Retrieves the metadata of a file from the resource DB,
+    as defined by the provided file_id param.
+    Attached to '/file_meta/<file_id>' route by flask annotation.
+
+    Parameters
+    ----------
+    file_id : string (UUID)
+        String representing the UUID of the metadata file `file_id`.
+
+    Returns
+    -------
+    Response (flask - JSON)
+        Generates flask Response object by converting dict to JSON.
+
+    """
     try:
         file_obj = META_DB.find_one({"id": uuid.UUID(file_id)}, {"_id": 0, "id": 0})
     except ValueError:
@@ -196,6 +333,20 @@ def get_file_meta(file_id):
 # the response would be massive, so paging would be needed here
 @APP.route('/all_filenames')
 def get_all_filenames():
+    """Fetches list of uploaded filenames from resource DB and
+    structures the returned list in JSON object.
+    Attached to '/all_filenames' route by flask annotation.
+
+    Parameters
+    ----------
+
+
+    Returns
+    -------
+    Response (flask - JSON)
+        Generates flask Response object by converting dict to JSON.
+
+    """
     all_files = []
     for resource in META_DB.find({}, {"id", "filename"}):
         all_files.append({"id": resource["id"], "filename": resource["filename"]})
@@ -208,6 +359,20 @@ def get_all_filenames():
 
 @APP.route('/abe/latest_mpk')
 def get_latest_mpk():
+    """Send latest Master Public Key (mpk) value as JSON,
+    by generating python dict payload and convering.
+    Attached to '/abe/latest_mpk' route by flask annotation.
+
+    Parameters
+    ----------
+
+
+    Returns
+    -------
+    Response (flask - JSON)
+        Generates flask Response object by converting dict to JSON.
+
+    """
     mpk_payload = {
         'generated_at': MASTER_KEY_UPDATED,
         'updated_at': datetime.now(),
@@ -218,6 +383,20 @@ def get_latest_mpk():
 
 @APP.route('/abe/latest_attributes')
 def get_latest_attributes():
+    """Send latest Global ABE Attributes (gaa) value as JSON,
+    by generating python dict payload and convering.
+    Attached to '/abe/latest_attributes' route by flask annotation.
+
+    Parameters
+    ----------
+
+
+    Returns
+    -------
+    Response (flask - JSON)
+        Generates flask Response object by converting dict to JSON.
+
+    """
     attributes_payload = {
         'generated_at': GLOBAL_ABE_ATTRS_UPDATED,
         'updated_at': datetime.now(),
