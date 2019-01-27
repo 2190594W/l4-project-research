da2a56c58de669be49ce4509d4b25948bb3ecd70
Chris Watson <2190594w@student.gla.ac.uk>
2019-01-27 18:43:10 +0000

    Cleanup MK server file for PyLint warnings. Added docstrings for all functions.

--- Code/mk_server/server.py
+++ Code/mk_server/server.py
@@ -34,22 +34,22 @@ except EnvironmentError:
     LOG.error("No sufficient session secret found, auto-generated random 64-bytes.")
     APP.secret_key = urandom(64)
 
-openabe = pyopenabe.PyOpenABE()
-cpabe_instance = openabe.CreateABEContext("CP-ABE")
+OPENABE = pyopenabe.PyOpenABE()
+CPABE = OPENABE.CreateABEContext("CP-ABE")
 
 try:
     with open(MASTER_SECRET_KEY_FILE, 'rb') as mskf:
         MASTER_SECRET_KEY = mskf.read()
     with open(MASTER_PUBLIC_KEY_FILE, 'rb') as mpkf:
         MASTER_PUBLIC_KEY = mpkf.read()
-    cpabe_instance.importSecretParams(MASTER_SECRET_KEY)
-    cpabe_instance.importPublicParams(MASTER_PUBLIC_KEY)
+    CPABE.importSecretParams(MASTER_SECRET_KEY)
+    CPABE.importPublicParams(MASTER_PUBLIC_KEY)
     MASTER_KEYS_GENERATED = datetime.fromtimestamp(path.getmtime(MASTER_SECRET_KEY_FILE))
 except EnvironmentError:
     try:
-        cpabe_instance.generateParams()
-        MASTER_SECRET_KEY = cpabe_instance.exportSecretParams()
-        MASTER_PUBLIC_KEY = cpabe_instance.exportPublicParams()
+        CPABE.generateParams()
+        MASTER_SECRET_KEY = CPABE.exportSecretParams()
+        MASTER_PUBLIC_KEY = CPABE.exportPublicParams()
         with open(MASTER_SECRET_KEY_FILE, 'wb') as mskf:
             mskf.write(MASTER_SECRET_KEY)
         with open(MASTER_PUBLIC_KEY_FILE, 'wb') as mpkf:
@@ -72,14 +72,41 @@ except EnvironmentError:
     GLOBAL_ABE_ATTRS_JSON_PICKLED = jsonpickle.encode(GLOBAL_ABE_ATTRS)
     GLOBAL_ABE_ATTRS_GENERATED = datetime.now()
 
-del cpabe_instance, openabe
+del CPABE, OPENABE
 
 @APP.route('/')
 def hello_world():
+    """Simple template generation for homepage/index of app.
+    Attached to '/' route by flask annotation.
+
+    Parameters
+    ----------
+
+
+    Returns
+    -------
+    Response (flask)
+        Generates flask Response object from Jinja2 template.
+
+    """
     return render_template('index.html')
 
 @APP.route('/abe/get_public_key')
 def get_public_key():
+    """Send latest Master Public Key (mpk) value as JSON,
+    by generating python dict payload and convering.
+    Attached to '/abe/get_public_key' route by flask annotation.
+
+    Parameters
+    ----------
+
+
+    Returns
+    -------
+    Response (flask - JSON)
+        Generates flask Response object by converting dict to JSON.
+
+    """
     mpk_payload = {
         'generated_at': MASTER_KEYS_GENERATED,
         'updated_at': datetime.now(),
@@ -90,6 +117,20 @@ def get_public_key():
 
 @APP.route('/abe/get_attributes')
 def get_attributes():
+    """Send latest Global ABE Attributes (gaa) value as JSON,
+    by generating python dict payload and convering.
+    Attached to '/abe/get_attributes' route by flask annotation.
+
+    Parameters
+    ----------
+
+
+    Returns
+    -------
+    Response (flask - JSON)
+        Generates flask Response object by converting dict to JSON.
+
+    """
     new_attrs = GLOBAL_ABE_ATTRS
     for attr in new_attrs:
         new_attrs[attr] = list(new_attrs[attr])
@@ -102,12 +143,44 @@ def get_attributes():
     return jsonify(attributes_payload)
 
 def assign_value_key(attr_type, key, value):
+    """Small function to provide the correct PyOpenABE separator
+    for an attribute, based on its type.
+
+    Parameters
+    ----------
+    attr_type : string
+        String for `attr_type` describing the type of the attribute.
+    key : string
+        String for the `key` of the attribute.
+    value : string
+        String representing the `value` of the attribute.
+
+    Returns
+    -------
+    string
+        Reformatted string equivalent of the attribute.
+
+    """
     attr_separators = {"strings": ":", "integers": "=", "dates": " = ", "arrays": ":"}
     separator = attr_separators[attr_type]
     value = f"|{key}{separator}".join(map(str, value)) if attr_type == "arrays" else value
     return f"{key}{separator}{value}"
 
 def create_attr_list(attrs):
+    """Create string 'list' of user attributes from a dictionary.
+    The returned string is just the PyOpenABE format for `attrs`.
+
+    Parameters
+    ----------
+    attrs : dict
+        Dictionary of `attrs` that are to be issued for a user key generation.
+
+    Returns
+    -------
+    string
+        String representing the `attrs` as an PyOpenABE compatible string.
+
+    """
     user_attr_list = "|"
     for attr_type, attr in attrs.items():
         if attr_type in ('strings', 'integers', 'dates', 'arrays'):
@@ -121,6 +194,19 @@ def create_attr_list(attrs):
     return user_attr_list
 
 def update_global_attrs(attrs):
+    """Given a dictionary of recently issued attributes, updates
+    the global list of attributes with any new attributes.
+
+    Parameters
+    ----------
+    attrs : dict
+        Dictionary of `attrs` that have just been issued during a user
+        key generation.
+
+    Returns
+    -------
+
+    """
     #pylint: disable=W0603
     global GLOBAL_ABE_ATTRS_JSON_PICKLED, GLOBAL_ABE_ATTRS_GENERATED
     for attr_type, attr in attrs.items():
@@ -136,13 +222,30 @@ def update_global_attrs(attrs):
 
 @APP.route('/abe/generate_userkey/<string:file_or_json>', methods=['POST'])
 def generate_userkey(file_or_json):
+    """Route for generating a new User Key.
+    Can either return the new Key as part of a JSON payload or as
+    a File attachment. Requires POST method, since the user's attributes
+    must be provided for generation.
+    Attached to '/abe/generate_userkey/<string:file_or_json>' route by flask annotation.
+
+    Parameters
+    ----------
+    file_or_json : string
+        String flag representing if the returned value should be a
+        BytesIO file or JSON string, `file_or_json`.
+
+    Returns
+    -------
+    Response (flask)
+        Generates flask Response object from a generated BytesIO file.
+
+    """
     if file_or_json not in ("file", "json"):
         abort(404)
     openabe_inst = pyopenabe.PyOpenABE()
     cpabe = openabe_inst.CreateABEContext("CP-ABE")
     cpabe.importSecretParams(MASTER_SECRET_KEY)
     cpabe.importPublicParams(MASTER_PUBLIC_KEY)
-    print("imported")
     user_attrs = request.get_json()
     if user_attrs is not None:
         if all(key in user_attrs for key in ('attributes', 'username')):
@@ -150,11 +253,10 @@ def generate_userkey(file_or_json):
             attrs = user_attrs['attributes']
             update_global_attrs(attrs)
             user_attr_list = create_attr_list(attrs)
-            print(user_attr_list)
             del user_attrs
             try:
                 cpabe.keygen(user_attr_list, str_username)
-                print("successfully generated key")
+                LOG.info("successfully generated key")
                 userkey_generated_at = datetime.now()
                 userkey = cpabe.exportUserKey(str_username)
             except pyopenabe.PyOpenABEError as err:
@@ -199,5 +301,5 @@ def page_not_found(error):
     if APP.env == 'development':
         template_error = error
     else:
-        print(error)
+        LOG.error("404 Error occurred: %s", error)
     return render_template('404.html', error=template_error), 404
