2b931691a8ed849d588d32b544fd26de1371cfa0
Chris Watson <2190594w@student.gla.ac.uk>
2019-01-30 22:59:20 +0000

    Improved extraction functions for further portability. Seems to work for >80% of cases now.

--- Code/crs_server/download_upload_tools.py
+++ Code/crs_server/download_upload_tools.py
@@ -44,9 +44,31 @@ def extract_policy(file_bytes):
         String representing the policy the resource was encrypted with.
 
     """
+    policy_regexes = [
+        br"(?:policy\w\W\w\s+)([\x1F-\x7F]+)",
+        br"(?:policy\w\W\w)([\x1F-\x7F]+)",
+        br"(?:policy\W+ )([\x1F-\x7F]+)",
+        br"(?:policy\W+ +)([\x1F-\x7F]+)",
+        br"(?:policy\W+ )(.+)(?:\W\/\W)",
+        br"(?:policy\w\W\w)(.+)(?:\W\/\W)",
+        br"(?:policy\W+ +)(.+\w+ [\:\w]+.)(?:[^ \s]{2,})",
+        br"(?:policy\w\W\w)(.+\w+ [\:\w]+)(?:[^\x00-\x7F \s]{2,})",
+        br"(?:policy\w\W\w)(.+\w+ [\:\w]+)(?:[\s]{1,})",
+        br"(?:policy\w\W\w\s+)(.+\w+ [\:\w]+)(?:[\s]{1,})"
+    ]
+    policy = ""
     file_contents = base64.b64decode(file_bytes)
-    re_policy = re.findall(br"(?:policy\W+ )(.+)(?:\W\/\W)", file_contents)
-    policy = re_policy[0].decode("utf-8")
+    for regex in policy_regexes:
+        re_policy = re.findall(regex, file_contents)
+        if not re_policy:
+            continue
+        try:
+            policy = re_policy[0].decode("utf-8")
+            break
+        except UnicodeDecodeError:
+            continue
+    if not policy:
+        policy = "Unknown"
     return policy
 
 def extract_user_attrs(key_bytes):
@@ -67,7 +89,24 @@ def extract_user_attrs(key_bytes):
         String representing the user's attributes.
 
     """
+    attr_regexes = [
+        br"(?:input\W+ *\|)([\x1F-\x7F]+)(?:\|)",
+        br"(?:input\W+\w+\|)([\x1F-\x7F]+)(?:\|)",
+        br"(?:input\W+ *\|)(.*)(?:\|$)",
+        br"(?:input\W+\w+\|)([\x00-\x7F]+)(?:\|$)",
+        br"(?:input\W+\w+\|)(.*)(?:\|$)"
+    ]
+    user_attrs = ""
     file_contents = base64.b64decode(key_bytes)
-    re_attrs = re.findall(br"(?:input\W+ *\|)(.*)(?:\|$)", file_contents)
-    user_attrs = re_attrs[0].decode("utf-8")
+    for regex in attr_regexes:
+        re_attrs = re.findall(regex, file_contents)
+        if not re_attrs:
+            continue
+        try:
+            user_attrs = re_attrs[0].decode("utf-8")
+            break
+        except UnicodeDecodeError:
+            continue
+    if not user_attrs:
+        user_attrs = "Unknown"
     return user_attrs
